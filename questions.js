e415b6f1b7efbab4e5a983c426d712387e9bb25d
Rodrigo Yuji <rodrigo.y.seto@gmail.com>
2025-08-20 20:10:56 +0900

    Initial commit with base files

---
+++ questions.js
@@ -0,0 +1,67 @@
+let questions = [
+  {
+    question: "In which HTML element do we place JavaScript?",
+    options: ["js tag", "script tag", "scripting tag", "javascript tag"],
+    answer: 1,
+  },
+  {
+    question: "Where is the correct place to insert JavaScript?",
+    options: [
+      "Inside the body tag",
+      "Inside the head tag",
+      "Both body and head tags are correct",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "What is the correct syntax to link an external JavaScript file in a script tag?",
+    options: ['name="xxx.js"', 'src="xxx.js"', 'href="xxx.js"'],
+    answer: 1,
+  },
+  {
+    question: "Does an external JavaScript file need the script tag?",
+    options: ["True", "False"],
+    answer: 1,
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    options: [
+      'alertBox("Hello World")',
+      'msgBox("Hello World")',
+      'msg("Hello World")',
+      'alert("Hello World")',
+    ],
+    answer: 3,
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    options: [
+      "function:minhaFuncao()",
+      "function = minhaFuncao()",
+      "function minhaFuncao()",
+    ],
+    answer: 2,
+  },
+  {
+    question: 'How do you call a function named "minhaFuncao"?',
+    options: ["call minhaFuncao", "call function minhaFuncao", "minhaFuncao()"],
+    answer: 2,
+  },
+  {
+    question: "How do you write an IF condition in JavaScript?",
+    options: ["if i = 5", "if i == 5 then", "if (i == 5)", "if i = 5 then"],
+    answer: 2,
+  },
+  {
+    question:
+      'How do you write an IF statement that executes code if "i" is not equal to 5?',
+    options: ["if (i != 5)", "if (i <> 5)", "if i <> 5", "if i =! 5 then"],
+    answer: 0,
+  },
+  {
+    question: "How does a while loop start?",
+    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
+    answer: 0,
+  },
+];
